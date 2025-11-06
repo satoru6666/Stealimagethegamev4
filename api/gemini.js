b69d3dd918df0f77e69e2e2b43e916af8694010b
satoru6666 <satriaprabhawa@gmail.com>
2025-11-07 00:43:12 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const GEMINI_KEY = process.env.GEMINI_API_KEY;
-  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_KEY;
+  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + GEMINI_KEY;
 
   const response = await fetch(url, {
     method: "POST",
