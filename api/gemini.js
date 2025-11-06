81b56e3a9fc6cb2818c4dbaab54baf4c54abdfe1
satoru6666 <satriaprabhawa@gmail.com>
2025-11-07 00:11:13 +0700

    Create gemini.js
    GEMINI API

---
+++ api/gemini.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const GEMINI_KEY = process.env.GEMINI_API_KEY;
+  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_KEY;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
